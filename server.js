67bf0004ed8adf3966a8bc4a204a156d3570f1d9
savalone47 <savalonescott47@gmail.com>
2020-06-30 22:26:01 +0200

    add dynamic post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const mongoose= require('mongoose');
+
 const bodyParser= require('body-parser');
 const cors = require('cors');
 
@@ -7,9 +8,13 @@ const PORT= process.env.PORT || 8000;
 const app= express();
 
 const User = require('./models/User');
+
 mongoose.connect('mongodb://localhost:27017/userData',
 	{ useUnifiedTopology: true,
 	  useNewUrlParser: true 
+	}, (err, data)=>{
+		if(err) throw err;
+		console.log('database is connected ');
 	});
 
 app.use(cors());
@@ -17,8 +22,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // CREATE
-app.post('/users',(req,res)=>{
-  // User.create()
+app.post('/users', (req, res) => {
+   User.create(
+   	{
+   		name: req.body.name,
+   		email: req.body.email,
+   		password: req.body.password
+
+   	},
+   	(err, data) => {
+   		if(err){
+   			res.json({success: false, message: err });
+   		}else if(!data){
+   			res.res.json({sucess: false, message: "Not found"});
+   		}else{
+   			res.json({sucess: true, data: data});
+   			console.log('request success');
+   		}
+   	})
 })
 
 app.route('/users/:id')
