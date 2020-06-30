ae1e3256b63f8dc4a908114f1bf4ec930052a7c2
savalone47 <savalonescott47@gmail.com>
2020-06-30 21:58:13 +0200

    add user model and connect to db

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
+const cors = require('cors');
 
 const PORT= process.env.PORT || 8000;
 const app= express();
 
 const User = require('./models/User');
-mongoose.connect('mongodb://localhost:27017/userData');
+mongoose.connect('mongodb://localhost:27017/userData',
+	{ useUnifiedTopology: true,
+	  useNewUrlParser: true 
+	});
 
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // CREATE
 app.post('/users',(req,res)=>{
   // User.create()
-});
+})
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
   // User.findById()
-});
+})
 // UPDATE
 .put((req,res)=>{
   // User.findByIdAndUpdate()
-});
+})
 // DELETE
 .delete((req,res)=>{
   // User.findByIdAndDelete()
-});
+})
 
-app.listen(PORT, () => console.log(`server is listening on port:${PORT}`);
+app.listen(PORT, () => console.log(`server is listening on port:${PORT}`));
