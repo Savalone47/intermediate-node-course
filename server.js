e560fccb64de7a384eb2ea043e7f2c4727dc7a7e
savalone47 <savalonescott47@gmail.com>
2020-06-30 21:29:21 +0200

    user model

--- server.js
+++ server.js
@@ -8,23 +8,23 @@ app.use(bodyParser.json());
 
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
-})
+});
 
 // CREATE
 app.post('/users',(req,res)=>{
   // User.create()
-})
+});
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
   // User.findById()
-})
+});
 // UPDATE
 .put((req,res)=>{
   // User.findByIdAndUpdate()
-})
+});
 // DELETE
 .delete((req,res)=>{
   // User.findByIdAndDelete()
-})
+});
